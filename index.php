7e18f87be73018e20888056adbc232dc0f373a99
saniya anand <sa2225@afsconnect1.njit.edu>
2017-11-19 16:22:09 -0500

    Modified

--- index.php
+++ index.php
@@ -181,7 +181,20 @@ abstract class model {
         $statement->execute();
     }
 }
+// Accounts table model
 class account extends model {
+    public $id;
+    public $email;
+    public $fname;
+    public $lname;
+    public $phone;
+    public $birthday;
+    public $gender;
+    public $password;
+    public static function getTablename(){
+        $tableName='accounts';
+        return $tableName;
+    }
 }
 class todo extends model {
     public $id;
